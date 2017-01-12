9ab26cec9dcdf0e7a6f6a331de692ddb1f21c319
HubertLegec <hubert.legec@gmail.com>
2017-01-12 23:49:38 +0100

    config

---
+++ StudentListAngular/Controllers/StudentListController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentListAngular.Controllers
+{
+    public class StudentListController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
