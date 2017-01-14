c3f5a30f1399f39cabb9718da0c7987c0ac8bf32
HubertLegec <hubert.legec@gmail.com>
2017-01-15 00:16:33 +0100

    API

---
+++ StudentListAngular/Controllers/GroupController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentListAngular.Controllers
+{
+    public class GroupController : Controller
+    {
+        private Storage s = new Storage();
+
+        public ActionResult Groups()
+        {
+            List<Group> groups = s.getGroups();
+            return Json(groups, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Add(Group group)
+        {
+            try
+            {
+                s.createGroup(group);
+                return Json(new { value = "OK"}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 400;
+                string error = "Nazwa grupy nie moze sie powtarzac!";
+                return Json(new { value = error }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Edit(Group group)
+        {
+            try
+            {
+                s.updateGroup(group);
+                return Json(new { value = "OK" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                var ex = e.GetBaseException();
+                if (ex.Message.ToLower().Contains("unique"))
+                {
+                    Response.StatusCode = 400;
+                    string error = "Nazwa grupy nie moze sie powtarzac!";
+                    return Json(new { value = error }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    Response.StatusCode = 409;
+                    string error = "Rekord zostal zmodyfikowany przez kogos innego!";
+                    return Json(new { value = error }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        public ActionResult Delete(Group group)
+        {
+            try
+            {
+                s.deleteGroup(group.IDGroup, group.Stamp);
+                return Json(new { value = "OK" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 409;
+                string error = "Rekord zostal zmodyfikowany przez kogos innego!";
+                return Json(new { value = error }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
--- StudentListAngular/Controllers/StudentListController.cs
+++ StudentListAngular/Controllers/StudentListController.cs
@@ -8,9 +8,93 @@ namespace StudentListAngular.Controllers
 {
     public class StudentListController : Controller
     {
+        private Storage s = new Storage();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Students()
+        {
+            List<Student> students = s.getStudents();
+            return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+        [ValidateInput(true)]
+        public ActionResult AddStudent(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    s.createStudent(student);
+                    return Json(new { value = "OK", JsonRequestBehavior.AllowGet });
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    string error = "Niepoprawne wartosci pol";
+                    return Json(new { value = error, JsonRequestBehavior.AllowGet });
+                }
+
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 400;
+                string error = "Numer indeksu musi byc unikalny!";
+                return Json(new { value = error, JsonRequestBehavior.AllowGet });
+            }
+        }
+
+        [ValidateInput(true)]
+        public ActionResult EditStudent(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    s.updateStudent(student);
+                    return Json(new { value = "OK", JsonRequestBehavior.AllowGet });
+                }
+                else
+                {
+                    Response.StatusCode = 400;
+                    string error = "Niepoprawne wartosci pol";
+                    return Json(new { value = error, JsonRequestBehavior.AllowGet });
+                }
+            }
+            catch (Exception e)
+            {
+                var ex = e.GetBaseException();
+                if (ex.Message.ToLower().Contains("unique"))
+                {
+                    Response.StatusCode = 400;
+                    string error = "Numer indeksu musi byc unikalny!";
+                    return Json(new { value = error, JsonRequestBehavior.AllowGet });
+                }
+                else
+                {
+                    Response.StatusCode = 409;
+                    string error = "Rekord zostal zmodyfikowany przez kogos innego!";
+                    return Json(new { value = error, JsonRequestBehavior.AllowGet });
+                }
+            }
+        }
+
+        public ActionResult DeleteStudent(Student student)
+        {
+            try
+            {
+                s.deleteStudent(student.IDStudent, student.Stamp);
+                return Json(new { value = "OK"}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                string error = "Rekord zostal zmodyfikowany przez kogos innego!";
+                Response.StatusCode = 409;
+                return Json(new { value = error}, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
